586a81516495841b309945d8e9806ae8036d2478
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2024-03-05 15:55:02 +0100

    added test for core desktop

---
+++ tests/installation/install_core_desktop.pm
@@ -0,0 +1,41 @@
+# Copyright (C) 2018 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+use base 'basetest';
+use strict;
+use testapi;
+
+use constant SLOW_TYPING_SPEED => 13;
+
+sub run {
+
+    assert_screen 'core-desktop-grub', 20;
+    #type_string 'ubuntu', SLOW_TYPING_SPEED;
+    #send_key 'ret';
+    #assert_screen 'mini_iso_logged_in', 10;
+
+}
+
+sub test_flags {
+    # 'fatal'          - abort whole test suite if this fails (and set overall state 'failed')
+    # 'ignore_failure' - if this module fails, it will not affect the overall result at all
+    # 'milestone'      - after this test succeeds, update 'lastgood'
+    # 'norollback'     - don't roll back to 'lastgood' snapshot if this fails
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
